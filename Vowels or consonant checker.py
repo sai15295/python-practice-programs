f71deea68af572e9a67f8bd0715bbcabbc188d26
sai15295 <ashrithasaiavula91@gmail.com>
2026-07-14 11:31:42 +0530

    Add vowel and consonant checker functionality
    Check if the entered character is vowel(a,e,i,o,u) or consonant using 'if-elif-else' condition

---
+++ Vowels or consonant checker.py
@@ -0,0 +1,13 @@
+Char = input("Enter the char: ")
+if char == 'a'or 'A':
+  print("vowel")
+elif char == 'e' or 'E':
+  print("vowel")
+elif char == 'i' or 'I':
+  print("vowel")
+elif char == 'o' or 'O':
+  print("vowel")
+elif char == 'u' or 'U':
+  print("vowel")
+else:
+  print("consonant")
